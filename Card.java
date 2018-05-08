6a3b2e19bd4928c9bfe5f8b912b5b849c73aba2d
RyodenV <36931040+RyodenV@users.noreply.github.com>
2018-05-08 12:54:53 -0400

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,79 @@
+import java.awt.Image;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+@SuppressWarnings("serial")
+public class Card implements Serializable{
+	
+	//Name
+	String name;
+	
+	//Cost/Colour
+	int[] cost = new int[6];
+	final String[] colours = new String[]{"Colourless", "White", "Blue", "Black", "Red", "Green"};
+	boolean[] colour = new boolean[] {true, false, false, false, false, false};
+	
+	//Type (Creature/Land/Instant/Sorcery/Enchantment/Artifact)
+	ArrayList<String> types = new ArrayList<String>();
+	
+	//Enters Tapped?
+	boolean entersTapped = false;
+	
+	//Image
+	Image pic;
+	
+	//Actives
+	
+	//Passives
+	
+	String flavourText;
+	
+	Card(){
+		name = "No Name";
+		for(int i = 0; i < cost.length; i++) {
+			cost[i] = 0;
+		}
+		types.add("No Type");
+		flavourText = "Flavour Text";
+	}
+	
+	Card(String nm, int[] cst, String type, String flavTxt, boolean tap, Image image){
+		name = nm;
+		cost = cst;
+		types.add(type);
+		flavourText = flavTxt;
+		entersTapped = tap;
+		pic = image;
+	}
+	
+	//Set Colours
+	public void setColours() {
+		for(int i = 1; i <= 5; i++) {
+			if(cost[i] > 0) {
+				addColour(i);
+			}
+		}
+	}
+	
+	//Add Colour
+	public void addColour(int i) {
+		if(i > 0 && i <=5) {
+			colour[i] = true;
+			colour[0] = false;
+		}
+	}
+	
+	//Add/Remove Type
+	public void addType(String t) {
+		types.add(t);
+	}
+	
+	public void removeType(String t) {
+		if(types.contains(t)) {
+			types.remove(t);
+		}
+	}
+	
+	//Add active
+	//Add passive
+}
---
+++ Creature.java
@@ -0,0 +1,143 @@
+import java.awt.Image;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+@SuppressWarnings("serial")
+public class Creature extends Card implements Serializable{
+	
+	//Creature Types
+	ArrayList<String> cType = new ArrayList<String>();
+	
+	//Perm and temp Pow+Tgh. Current HP and +1/+1 counters 
+	int power, toughness, tempP, tempT, hp, oneones;
+	
+	//Keywords
+	static final String[] keywords = {"Vigilance","Haste", "LifeLink", "First Strike", "Double Strike", "Hexproof", "Flying", "Reach", "Deathtouch"};
+	ArrayList<Integer> activeKeywords = new ArrayList<Integer>(); 
+	
+	//Auras/Equipment
+	
+	Creature(){
+		super();
+		power = 0;
+		toughness = 0;
+		tempP = 0;
+		tempT = 0;
+		hp = 0;
+		oneones = 0;
+	}
+	
+	Creature(String nm, int[] cst, String type, String flavTxt, boolean tap, Image image, int pow, int tgh, int oo){
+		super(nm, cst, "Creature", flavTxt, tap, image);
+		cType.add(type);
+		power = pow;
+		toughness = tgh;
+		tempP = 0;
+		tempT = 0;
+		hp = tgh;
+		oneones = oo;
+	}
+	
+	public boolean takeDamage(int dmg) {
+		//take damage
+		hp -= dmg;
+		
+		//return if creature is alive
+		return hp>0;
+	}
+	
+	public void healDamage() {
+		//reset to max health
+		hp = toughness + oneones;
+	}
+	
+	//Add/Remove Aura+Enchment
+	
+	//Add/Remove one ones
+	public void addOneOnes(int counters) {
+		oneones += counters;
+	}
+	
+	public void removeOneOnes(int counters) {
+		oneones -= counters;
+	}
+	
+	//Add Temp P/T
+	public void addTempPT(int pow, int tgh) {
+		tempP += pow;
+		tempT += tgh;
+		hp += tgh;
+	}
+	
+	//Clear Temp P/T
+	public void clearTempPT() {
+		tempP = 0;
+		tempT = 0;
+		healDamage();
+	}
+	
+	//Add creature type
+	public void addType(String type) {
+		cType.add(type);
+	}
+	
+	//Remove creature type
+	public void removeType(String type) {
+		cType.remove(type);
+	}
+	
+	//Add keyword
+	public void addKeyword(int keyword) {
+		activeKeywords.add(keyword);
+	}
+	
+	//Remove keyword
+	public void removeKeyword(String keyword) {
+		for(int i = 0; i<keywords.length; i++) {
+			if(keywords[i].equals(keyword) && activeKeywords.contains(i)) {
+				activeKeywords.remove(i);
+			}
+		}
+	}
+	
+	public String toString() {
+		String output = this.name + " is a ";
+		String cost = "";
+		String kwrds = "";
+		
+		output += this.power + "/" + this.toughness + " ";
+		
+		for(int i = 0; i < this.colour.length; i++) {
+			if(this.colour[i]) {
+				output += this.colours[i] + ", ";
+			}
+			if(this.cost[i] > 0) {
+				cost += this.cost[i] + " " + this.colours[i] + ", ";
+			}
+		}
+		
+		for(int i = 0; i < this.types.size(); i++) {
+			output += this.cType.get(i) + ", ";
+		}
+		
+		output += "that costs " + cost;
+		output = output.substring(0, output.length()-2);
+		output += ". It has: ";
+		
+		
+		for(int i = 0; i < this.activeKeywords.size(); i++) {
+			if(!kwrds.contains(Creature.keywords[this.activeKeywords.get(i)])) {
+				kwrds += Creature.keywords[this.activeKeywords.get(i)] + ", ";
+			}
+		}
+		if(kwrds.length() > 3) {
+			kwrds = kwrds.substring(0, kwrds.length()-2);
+			output += kwrds;
+		} else {
+			output = output.substring(0, output.length()-9);
+		}
+		
+		
+		return output;
+	}
+}
